0cd7410ca0de319f2e57e56ba6cf3ca6e0b11e26
daniel-pohl <danielpohl@mail.de>
2024-06-14 18:51:39 +0200

    add put option (direct into the existing todo)

--- frontend/src/TodoCard/TodoCard.tsx
+++ frontend/src/TodoCard/TodoCard.tsx
@@ -1,26 +1,37 @@
 import {Todo} from "./Todo.ts";
 import './TodoCard.css';
 import axios from "axios";
+import {useState} from "react";
 
 type Props = {
     todo: Todo,
     onNewTodoItemChange: () => void,
 }
 
-
 function TodoCard(props: Props) {
+
+    const [description, setDescription] = useState(props.todo.description);
 
     function deleteThisItem() {
         axios.delete("api/todo/" + props.todo.id)
             .then(props.onNewTodoItemChange)
+    }
+    function changeText(event: React.ChangeEvent<HTMLInputElement>) {
+        const newDescription = event.target.value;
+        setDescription(newDescription);
+        axios.put("api/todo/" + props.todo.id, {
+            id: props.todo.id,
+            description: newDescription,
+            status: props.todo.status,
+        } as Todo)
 
     }
-
-
     return (
         <div className="todo-card">
-            {props.todo.description}
-            <button className="delete-button" onClick={deleteThisItem}>🗑️</button>
+
+            <input value={description} onInput={changeText}
+            />
+            <button onClick={deleteThisItem} className="delete-button">🗑️</button>
         </div>
     );
 }
